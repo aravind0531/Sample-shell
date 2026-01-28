ee78d8baa2a2a338f2052ee70399bdca38d8339d
infosec0531 <infosec.aravind@gmail.com>
2026-01-28 21:17:44 +0530

    adding sample file

---
+++ varibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+#basic Shell
+echo "$PERSON1:: This is devops class notes"
+echo "$PERSON2:: I know we cam start the notes"
+echo "$PERSON1:: We can start shell Scripting"
+echo "$PERSON2:: We can start shell Scripting"
+echo "$PERSON1:: We can start shell Scripting"
